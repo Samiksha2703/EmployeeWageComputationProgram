9af312d24c01f1b8afdc460d1b9af67edc58ac8d
samiksha Shende <shende.samiksha@rediffmail.com>
2020-09-29 11:38:42 +0530

    Added : Daily Wage Code Added

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,10 +1,22 @@
 ##!/bin/bash/ -x
 echo "Welcome to Employee Wage Computation Program"
 echo "Attendance"
-R=$(( $RANDOM % 2 ))
-if [ $R -eq 0 ]
+R1=$(( $RANDOM % 2 ))
+if [ $R1 -eq 0 ]
 then
 echo "A"
 else
 echo "P"
+R2=$(( $RANDOM % 8 + 1 ))
+if [ $R2 -eq 8 ]
+then
+echo "Full Day"
+elif [ $R2 -eq 4 ]
+then
+echo "Half Day"
+else
+echo $R2
+W=$(( $R2 * 20 ))
+echo "DailyWage : $W"
+fi
 fi
